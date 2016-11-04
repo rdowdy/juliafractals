662d7825584f7be492bf6e2e6f7bc193216ba411
Ryan Dowdy <rdowdy21@gmail.com>
2016-11-03 22:38:29 -0700

    Console log mouse coordinates relative to canvas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("canvas");
+var width = canvas.width;
+var height = canvas.height;
 
 canvas.onmousemove = function(e) {
-	console.log(e);
+	console.log("x: " + e.layerX);
+	console.log("y: " + e.layerY / height);
 }
